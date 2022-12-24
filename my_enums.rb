5ff528f04895815a9559b11d378a9b129ba983a2
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-12-24 14:59:26 +0600

    Add any? filter methods

--- my_enums.rb
+++ my_enums.rb
@@ -1,8 +1,23 @@
 module MyEnumerable
-
-def all?
+  def all?
     res = true
     each { |i| return res = false unless yield i }
     print res
-end
+  end
 
+  def any?
+    res = false
+    each do |i|
+      return res = true if yield i
+    end
+    print res
+  end
+
+  def filter
+    res = []
+    each do |i|
+      res.push i if yield i
+    end
+    return res
+  end
+end
